6ea3bd27ba387bfd884d6deaf163292f54922436
rgosens2 <rgosens2@gmail.com>
2022-01-06 18:23:15 +0100

    all black

--- toc.py
+++ toc.py
@@ -1,11 +1,14 @@
 # generate a table of contents from <h2> headers, and replace the <!-- h2toc --> comment with it
 import re
-h2re = re.compile('<h2>(.*)</h2>')
-def gentoc(filename,name,visname=None):
-    if visname==None:
+
+h2re = re.compile("<h2>(.*)</h2>")
+
+
+def gentoc(filename, name, visname=None):
+    if visname == None:
         visname = filename
-    f=open(filename)
-    orig=f.read()
+    f = open(filename)
+    orig = f.read()
     f.close()
 
     headings = []
@@ -13,26 +16,30 @@ def gentoc(filename,name,visname=None):
 
     c = 1
     where = None
-    for line in orig.split('\n'):
+    for line in orig.split("\n"):
         m = h2re.match(line)
-        if '<!-- h2toc -->' in line:
-            where = len(changed) # mark the pos where the toc must be generated
+        if "<!-- h2toc -->" in line:
+            where = len(changed)  # mark the pos where the toc must be generated
         if m:
             headings.append(m.groups()[0])
-            line = '<a name="%s-%d"></a>'%(name,c) + line
+            line = '<a name="%s-%d"></a>' % (name, c) + line
             c += 1
         changed.append(line)
 
     if where == None:
-        print('no <!-- h2toc --> comment found in',filename,'- table of contents not generated')
+        print(
+            "no <!-- h2toc --> comment found in",
+            filename,
+            "- table of contents not generated",
+        )
         return
 
-    toc = '<ul>\n'
-    for i,subj in enumerate(headings):
-        toc += '<li><a href="%s#%s-%d">%s</a></li>\n'%(visname,name,i+1,subj)
-    toc += '</ul>'
+    toc = "<ul>\n"
+    for i, subj in enumerate(headings):
+        toc += '<li><a href="%s#%s-%d">%s</a></li>\n' % (visname, name, i + 1, subj)
+    toc += "</ul>"
     changed[where] = toc
 
-    f = open(filename,'w')
-    f.write('\n'.join(changed))
+    f = open(filename, "w")
+    f.write("\n".join(changed))
     f.close()
--- upload.py
+++ upload.py
@@ -1,14 +1,15 @@
 import ftplib
 import os
 
-fqas = [f for f in os.listdir('.') if f.endswith('.fqa')]
-htmls = [f[:-4] + '.html' for f in fqas] + ['fqa.html']
+fqas = [f for f in os.listdir(".") if f.endswith(".fqa")]
+htmls = [f[:-4] + ".html" for f in fqas] + ["fqa.html"]
 print(htmls)
 
-files = htmls#['index.html']#['defective.html','mixing.html']#htmls
+files = htmls  # ['index.html']#['defective.html','mixing.html']#htmls
 
-os.system('zip fqa.zip %s'%' '.join(files))
+os.system("zip fqa.zip %s" % " ".join(files))
 
-print('upload and unzip fqa.zip')
+print("upload and unzip fqa.zip")
 import sys
+
 sys.exit()
